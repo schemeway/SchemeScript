3aff92817396c88e41c2dcde5f6751f2c8f3e08b
schemeway <schemeway>
2004-09-30 13:51:44 +0000

    initial revision

---
+++ src/org/schemeway/plugins/schemescript/dialogs/SymbolSelectionDialog.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2004 Nu Echo Inc.
+ * 
+ * This is free software. For terms and warranty disclaimer, see ./COPYING
+ */
+package org.schemeway.plugins.schemescript.dialogs;
+
+import org.eclipse.jface.window.*;
+import org.eclipse.swt.*;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
+import org.schemeway.plugins.schemescript.dictionary.*;
+
+public class SymbolSelectionDialog extends Window {
+    private SymbolEntry[] mEntries;
+    private SymbolEntry mSelectedEntry = null;
+
+    public SymbolSelectionDialog(Shell parentShell, SymbolEntry[] entries) {
+        super(parentShell);
+        setShellStyle(SWT.APPLICATION_MODAL);
+        setBlockOnOpen(true);
+        mEntries = entries;
+        if (entries.length > 0) {
+            mSelectedEntry = mEntries[0];
+        }
+    }
+
+    public SymbolEntry getSelectedEntry() {
+        return mSelectedEntry;
+    }
+
+    protected Control createContents(Composite parent) {
+        Composite composite = new Composite(parent, SWT.NULL);
+        GridLayout layout = new GridLayout(1, true);
+        layout.verticalSpacing = 0;
+        layout.marginHeight = 0;
+        layout.marginWidth = 0;
+        composite.setLayout(layout);
+        
+        Label label = new Label(composite, SWT.SHADOW_OUT);
+        label.setText("Symbols");
+        GridData data = new GridData(GridData.FILL_HORIZONTAL);
+        label.setLayoutData(data);
+        
+        
+        final List list = new List(composite, SWT.SINGLE);
+        data = new GridData(GridData.FILL_BOTH);
+        list.setLayoutData(data);
+
+        for (int i = 0; i < mEntries.length; i++) {
+            list.add(mEntries[i].getDescription() + " [" + mEntries[i].getContext() + "]");
+        }
+        list.select(0);
+
+        list.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent event) {
+                int index = list.getSelectionIndex();
+                mSelectedEntry = mEntries[index];
+            }
+        });
+        list.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent event) {
+                if (event.character == '\r') {
+                    ok();
+                }
+                else
+                    if (event.character == 27) {
+                        cancel();
+                    }
+            }
+        });
+        list.addMouseListener(new MouseAdapter() {
+            public void mouseDoubleClick(MouseEvent event) {
+                ok();
+            }
+        });
+        return composite;
+    }
+
+    protected void ok() {
+        close();
+    }
+
+    protected void cancel() {
+        mSelectedEntry = null;
+        close();
+    }
+}
---
+++ src/org/schemeway/plugins/schemescript/dictionary/IUserDictionary.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2002-2003 Nu Echo Inc. All rights reserved.
+ */
+package org.schemeway.plugins.schemescript.dictionary;
+
+/**
+ * @author Nu Echo Inc.
+ */
+public interface IUserDictionary {
+    void addEntry(SymbolEntry entry);
+    void removeEntry(SymbolEntry entry);
+}
