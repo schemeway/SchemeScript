8dc7e1a1ce0f3da287e9247596e95607a2dfe590
schemeway <schemeway>
2004-11-03 18:23:05 +0000

    Find Definition action now uses the Definition List view

--- src/org/schemeway/plugins/schemescript/action/JumpToDefinitionAction.java
+++ src/org/schemeway/plugins/schemescript/action/JumpToDefinitionAction.java
@@ -6,22 +6,15 @@
 package org.schemeway.plugins.schemescript.action;
 
 import java.util.*;
-import java.util.List;
-
-import javax.swing.text.*;
 
 import org.eclipse.core.resources.*;
 import org.eclipse.jface.action.*;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.window.*;
-import org.eclipse.swt.widgets.*;
 import org.eclipse.ui.*;
-import org.eclipse.ui.ide.*;
 import org.eclipse.ui.part.*;
 import org.schemeway.plugins.schemescript.*;
-import org.schemeway.plugins.schemescript.dialogs.*;
 import org.schemeway.plugins.schemescript.dictionary.*;
 import org.schemeway.plugins.schemescript.editor.*;
+import org.schemeway.plugins.schemescript.views.*;
 
 public class JumpToDefinitionAction extends Action {
     private SchemeEditor mEditor;
@@ -33,14 +26,14 @@ public class JumpToDefinitionAction extends Action {
         mEditor = editor;
         setupResource();
     }
-    
+
     private void setupResource() {
         IEditorInput input = mEditor.getEditorInput();
         if (input instanceof FileEditorInput) {
-            mResource = ((FileEditorInput)input).getFile();
+            mResource = ((FileEditorInput) input).getFile();
         }
     }
-    
+
     private IResource getResource() {
         return mResource;
     }
@@ -63,25 +56,14 @@ public class JumpToDefinitionAction extends Action {
                 }
                 else {
                     entries = boostPriorities(entries);
-                    Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-                    SymbolSelectionDialog dialog = new SymbolSelectionDialog(shell, entries);
-                    dialog.open();
-                    if (dialog.getReturnCode() == Window.OK)
-                        entry = dialog.getSelectedEntry();
+                    showInView(entries);
                 }
-                
+
                 if (entry != null && entry.getFile() == null)
                     entry = null;
 
                 if (entry != null) {
-                    IFile file = entry.getFile();
-                    int linenumber = entry.getLineNumber();
-                    try {
-                        openEditorAtLine(file, linenumber - 1);
-                    }
-                    catch (PartInitException exception) {
-                        SchemeScriptPlugin.logException("Exception in jump definition", exception);
-                    }
+                    DefinitionListView.openEditorAtLine(entry);
                 }
             }
         }
@@ -89,46 +71,38 @@ public class JumpToDefinitionAction extends Action {
             SchemeScriptPlugin.logException("Exception in jump definition", exception);
         }
     }
-    
-    private void openEditorAtLine(IFile file, int linenumber) throws PartInitException
-    {
-        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-        IEditorPart editor = IDE.openEditor(page, file, true);
-        if (editor == null) {
-            return;
-        }
-        
-        if (editor instanceof SchemeEditor) {
-            SchemeEditor schemeEditor = (SchemeEditor)editor;
-            try {
-                int lineStart = schemeEditor.getDocument().getLineOffset(linenumber);
-                int lineEnd   = lineStart + schemeEditor.getDocument().getLineLength(linenumber);
-                schemeEditor.setSelection(lineStart, lineEnd);
-            }
-            catch (BadLocationException exception) {
-            }
-        }
+
+    private void showInView(SymbolEntry[] entries) throws PartInitException {
+        DefinitionListView view = (DefinitionListView) PlatformUI.getWorkbench()
+                                                                 .getActiveWorkbenchWindow()
+                                                                 .getActivePage()
+                                                                 .showView(DefinitionListView.DEFINITION_LIST_ID);
+        view.setEntries(entries);
+        view.setFocus();
     }
 
     private SymbolEntry[] boostPriorities(SymbolEntry[] entries) {
         List list = Arrays.asList(entries);
-        
+
         Collections.sort(list, new Comparator() {
             public int compare(Object o1, Object o2) {
-                SymbolEntry e1 = (SymbolEntry)o1;
-                SymbolEntry e2 = (SymbolEntry)o2;
+                SymbolEntry e1 = (SymbolEntry) o1;
+                SymbolEntry e2 = (SymbolEntry) o2;
                 int p1 = e1.getPriority();
                 int p2 = e2.getPriority();
                 if (e1.getFile() != null && e1.getFile().equals(getResource()))
                     p1 += 10;
                 if (e2.getFile() != null && e2.getFile().equals(getResource()))
                     p2 += 10;
-                if (p1 < p2) return 1;
-                if (p1 == p2) return 0;
-                else return -1;
+                if (p1 < p2)
+                    return 1;
+                if (p1 == p2)
+                    return 0;
+                else
+                    return -1;
             }
         });
-        
+
         return (SymbolEntry[]) list.toArray(new SymbolEntry[list.size()]);
     }
 }
---
+++ src/org/schemeway/plugins/schemescript/views/DefinitionListView.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2004 Nu Echo Inc.
+ * 
+ * This is free software. For terms and warranty disclaimer, see ./COPYING
+ */
+package org.schemeway.plugins.schemescript.views;
+
+import org.eclipse.core.resources.*;
+import org.eclipse.jface.text.*;
+import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.ui.*;
+import org.eclipse.ui.ide.*;
+import org.eclipse.ui.part.*;
+import org.schemeway.plugins.schemescript.*;
+import org.schemeway.plugins.schemescript.dictionary.*;
+import org.schemeway.plugins.schemescript.editor.*;
+
+public class DefinitionListView extends ViewPart {
+
+    public final static String DEFINITION_LIST_ID = SchemeScriptPlugin.PLUGIN_NS + ".views.definitionList";
+
+    private static class SymbolLabelProvider extends LabelProvider implements ITableLabelProvider {
+        public String getColumnText(Object obj, int index) {
+            switch (index) {
+                case 0:
+                    return "";
+                case 1:
+                    return ((SymbolEntry) obj).getDescription();
+                case 2:
+                    return ((SymbolEntry) obj).getFile().getRawLocation().toOSString();
+                default:
+                    return "";
+            }
+        }
+
+        public Image getColumnImage(Object obj, int index) {
+            return null;
+        }
+    }
+
+    private SymbolEntry[] mShownEntries = new SymbolEntry[0];
+    private TableViewer mViewer;
+
+    public DefinitionListView() {
+        super();
+    }
+
+    public void createPartControl(Composite parent) {
+        mViewer = new TableViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.SINGLE | SWT.FULL_SELECTION);
+        final Table table = mViewer.getTable();
+
+        TableColumn typeColumn = new TableColumn(table, SWT.LEFT);
+        typeColumn.setText("");
+        typeColumn.setWidth(18);
+
+        TableColumn categoryColumn = new TableColumn(table, SWT.LEFT);
+        categoryColumn.setText("Description");
+        categoryColumn.setWidth(200);
+
+        TableColumn locationColumn = new TableColumn(table, SWT.LEFT);
+        locationColumn.setText("Location");
+        locationColumn.setWidth(450);
+
+        table.setHeaderVisible(true);
+        table.setLinesVisible(true);
+
+        mViewer.setContentProvider(new ArrayContentProvider());
+        mViewer.setLabelProvider(new SymbolLabelProvider());
+        mViewer.setInput(mShownEntries);
+
+        mViewer.addDoubleClickListener(new IDoubleClickListener() {
+            public void doubleClick(DoubleClickEvent event) {
+                IStructuredSelection selection = (IStructuredSelection) mViewer.getSelection();
+                if (!selection.isEmpty()) {
+                    openEditorAtLine((SymbolEntry) selection.getFirstElement());
+                }
+            }
+        });
+    }
+
+    public void setFocus() {
+        mViewer.getTable().setFocus();
+    }
+
+    public void setEntries(SymbolEntry[] entries) {
+        mShownEntries = entries;
+        updateTable();
+    }
+
+    private void updateTable() {
+        mViewer.getTable().setRedraw(false);
+        mViewer.setInput(mShownEntries);
+        if (mShownEntries.length > 0) {
+            mViewer.setSelection(new StructuredSelection(mShownEntries[0]), true);
+        }
+        mViewer.getTable().setRedraw(true);
+    }
+
+    public static void openEditorAtLine(SymbolEntry entry) {
+        if (entry == null)
+            return;
+
+        try {
+            IFile file = entry.getFile();
+            int linenumber = entry.getLineNumber() - 1;
+
+            IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+            IEditorPart editor = IDE.openEditor(page, file, true);
+            if (editor == null) {
+                return;
+            }
+
+            if (editor instanceof SchemeEditor) {
+                SchemeEditor schemeEditor = (SchemeEditor) editor;
+                try {
+                    int lineStart = schemeEditor.getDocument().getLineOffset(linenumber);
+                    int lineEnd = lineStart + schemeEditor.getDocument().getLineLength(linenumber);
+                    schemeEditor.setSelection(lineStart, lineEnd);
+                }
+                catch (BadLocationException exception) {
+                }
+            }
+        }
+        catch (PartInitException exception) {
+            SchemeScriptPlugin.logException("Unable to open resource", exception);
+        }
+    }
+}
