2b76b7cf1f92aac07ed0592c53f25103aed182a2
schemeway <schemeway>
2004-11-16 21:45:33 +0000

    enhanced perspective

--- src/org/schemeway/plugins/schemescript/SchemePerspective.java
+++ src/org/schemeway/plugins/schemescript/SchemePerspective.java
@@ -15,10 +15,36 @@ public class SchemePerspective implements IPerspectiveFactory {
     }
 
     public void createInitialLayout(IPageLayout layout) {
+        defineActions(layout);
+        defineLayout(layout);
+    }
+    
+    private void defineLayout(IPageLayout layout) {
         String editorArea = layout.getEditorArea();
         
-        layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.LEFT, 0.20f, editorArea);
+        IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, 0.20f, editorArea);
+        topLeft.addView(IPageLayout.ID_RES_NAV);
+        
+        IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.50f, "topLeft");
+        bottomLeft.addPlaceholder(IPageLayout.ID_OUTLINE);
         
         layout.addFastView(DefinitionListView.DEFINITION_LIST_ID);
+        layout.addFastView("org.eclipse.ui.console.ConsoleView");
+    }
+
+    private void defineActions(IPageLayout layout) {
+        // Add "new wizards".
+        layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");//$NON-NLS-1$
+        layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");//$NON-NLS-1$
+
+        // Add "show views".
+        layout.addShowViewShortcut(IPageLayout.ID_RES_NAV);
+        layout.addShowViewShortcut(IPageLayout.ID_BOOKMARKS);
+        layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+        layout.addShowViewShortcut(IPageLayout.ID_PROP_SHEET);
+        layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+        layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+        
+        layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
     }
 }
