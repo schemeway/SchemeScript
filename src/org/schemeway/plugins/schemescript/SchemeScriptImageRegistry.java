7423d52e163706b97c76a1833c1edda9f82d7366
schemeway <schemeway>
2005-02-01 16:08:38 +0000

    initial revision

---
+++ src/org/schemeway/plugins/schemescript/SchemeScriptImageRegistry.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2004 Nu Echo Inc.
+ * 
+ * This is free software. For terms and warranty disclaimer, see ./COPYING
+ */
+package org.schemeway.plugins.schemescript;
+
+import org.eclipse.core.runtime.*;
+import org.eclipse.jface.resource.*;
+import org.eclipse.swt.graphics.*;
+
+public final class SchemeScriptImageRegistry {
+
+    private SchemeScriptImageRegistry() {
+    }
+
+    // finds an image in the Plugin
+    public static Image getImage(String relativePath) {
+
+        ImageDescriptor descriptor = getImageDescriptor(relativePath);
+        if (descriptor == null)
+            return null;
+        return descriptor.createImage();
+    }
+
+    public static ImageDescriptor getImageDescriptor(String relativePath) {
+        return ImageDescriptor.createFromURL(SchemeScriptPlugin.getDefault().find(new Path(relativePath)));
+    }
+}
