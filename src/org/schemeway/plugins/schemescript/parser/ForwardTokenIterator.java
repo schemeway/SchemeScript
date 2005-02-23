bc57d0532e6ca911921fda59f34c72658accf047
schemeway <schemeway>
2005-02-23 14:28:41 +0000

    Fixed 'setPosition' for proper end of buffer handling

--- src/org/schemeway/plugins/schemescript/parser/ForwardTokenIterator.java
+++ src/org/schemeway/plugins/schemescript/parser/ForwardTokenIterator.java
@@ -80,6 +80,7 @@ public class ForwardTokenIterator implements ISchemeTokenIterator {
                     }
             }
             else {
+                mPosition = mEnd = position;
                 mCurrentPartition = null;
             }
         }
