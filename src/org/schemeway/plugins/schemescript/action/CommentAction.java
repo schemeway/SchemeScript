956693b62239859eadffd43a859b598137520a7d
schemeway <schemeway>
2006-07-11 14:29:59 +0000

    Can now remove other types of comments

--- src/org/schemeway/plugins/schemescript/action/CommentAction.java
+++ src/org/schemeway/plugins/schemescript/action/CommentAction.java
@@ -10,55 +10,90 @@ import org.schemeway.plugins.schemescript.editor.*;
 import org.schemeway.plugins.schemescript.preferences.*;
 
 public class CommentAction extends SchemeAction {
-    
-    public CommentAction(SchemeEditor editor) {
-        super(editor);
-        setText("Toggle comment");
-        setToolTipText("Comment/Uncomment the selected lines");
-    }
 
-    public void run() {
-        SchemeEditor editor = getSchemeEditor();
-        if (editor == null) return;
-        
-        try {
-            
-            final IDocument document = editor.getDocument();
-            Region selection = editor.getSelection();
+	public CommentAction(SchemeEditor editor) {
+		super(editor);
+		setText("Toggle comment");
+		setToolTipText("Comment/Uncomment the selected lines");
+	}
 
-            final int startLine = document.getLineOfOffset(selection.getOffset());
-            final int endLine = document.getLineOfOffset(selection.getOffset() + selection.getLength());
-            editor.runCompoundChange(new Runnable() {
-                public void run() {
-                    processLines(document, startLine, endLine);
-                }
-            });
-        }
-        catch (BadLocationException exception) {
-        }
-    }
+	public void run() {
+		SchemeEditor editor = getSchemeEditor();
+		if (editor == null)
+			return;
 
-    private static void processLines(IDocument document, int startLine, int endLine) {
-        String prefix = CommentPreferences.getCommentPrefix();
-        int prefixLength = prefix.length();
+		try {
+			final IDocument document = editor.getDocument();
+			final Region selection = editor.getSelection();
 
-        try {
-            for (int line = startLine; line <= endLine; line++) {
-                IRegion lineInfo = document.getLineInformation(line);
-                int lineOffset = lineInfo.getOffset();
-                int lineLength = lineInfo.getLength();
+			final int startLine = document.getLineOfOffset(selection.getOffset());
+			final int endLine = document.getLineOfOffset(selection.getOffset() + selection.getLength());
+			if (!(document.getPartition(selection.getOffset()).getType() == SchemePartitionScanner.SCHEME_COMMENT)
+					|| (selection.getLength() != 0)) {
+				editor.runCompoundChange(new Runnable() {
+					public void run() {
+						processLines(document, startLine, endLine);
+					}
+				});
+			}
+			else {
+				editor.runCompoundChange(new Runnable() {
+					public void run() {
+						removeCommentFromPartition(document, selection.getOffset());
+					}
+				});
+			}
+		}
+		catch (BadLocationException exception) {
+		}
+	}
 
-                if (lineLength >= prefixLength && document.get(lineOffset, prefixLength).equals(prefix)) {
-                    document.replace(lineOffset, prefixLength, "");
-                }
-                else {
-                    if (lineLength > 0) {
-                        document.replace(lineOffset, 0, prefix);
-                    }
-                }
-            }
-        }
-        catch (BadLocationException exception) {
-        }
-    }
+	private void removeCommentFromPartition(IDocument document, int offset) {
+		try {
+			ITypedRegion partition = document.getPartition(offset);
+			
+			if (partition.getType() == SchemePartitionScanner.SCHEME_COMMENT) {
+				int partitionOffset = partition.getOffset();
+				if (document.getChar(partitionOffset) == ';') {
+					int commentPrefixEnd = partitionOffset;
+					while (document.getChar(commentPrefixEnd) == ';') 
+						commentPrefixEnd++;
+					document.replace(partitionOffset, commentPrefixEnd - partitionOffset, "");
+				}
+				else if (document.get(partitionOffset, 2).equals("#|")) {
+					document.replace(partitionOffset + partition.getLength() - 2, 2, "");
+					document.replace(partitionOffset, 2, "");
+				}
+				else if (document.get(partitionOffset, 2).equals("#;")) {
+					document.replace(partitionOffset, 2, "");
+				}
+			}
+		}
+		catch (BadLocationException e) {
+		}
+	}
+
+	private static void processLines(IDocument document, int startLine, int endLine) {
+		String prefix = CommentPreferences.getCommentPrefix();
+		int prefixLength = prefix.length();
+
+		try {
+			for (int line = startLine; line <= endLine; line++) {
+				IRegion lineInfo = document.getLineInformation(line);
+				int lineOffset = lineInfo.getOffset();
+				int lineLength = lineInfo.getLength();
+
+				if (lineLength >= prefixLength && document.get(lineOffset, prefixLength).equals(prefix)) {
+					document.replace(lineOffset, prefixLength, "");
+				}
+				else {
+					if (lineLength > 0) {
+						document.replace(lineOffset, 0, prefix);
+					}
+				}
+			}
+		}
+		catch (BadLocationException exception) {
+		}
+	}
 }
