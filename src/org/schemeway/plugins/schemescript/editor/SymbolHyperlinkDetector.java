870b942309011487ce2ede2a2c4ab1945df77c3e
schemeway <schemeway>
2007-11-19 02:08:53 +0000

    Java classes can be opened by ctrl-clicking on them.

--- src/org/schemeway/plugins/schemescript/editor/SymbolHyperlinkDetector.java
+++ src/org/schemeway/plugins/schemescript/editor/SymbolHyperlinkDetector.java
@@ -4,7 +4,11 @@
 package org.schemeway.plugins.schemescript.editor;
 
 import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.*;
+import org.eclipse.jdt.core.*;
+import org.eclipse.jdt.ui.*;
 import org.eclipse.jface.text.*;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.hyperlink.*;
 import org.eclipse.ui.*;
 import org.eclipse.ui.part.*;
@@ -37,10 +41,49 @@ public class SymbolHyperlinkDetector implements IHyperlinkDetector {
 			if (entries.length > 0) {
 				return new IHyperlink[] { new SymbolHyperlink(symbolRegion, symbol, entries) };
 			}
+
+			if (isTypeName(symbol)) {
+				IJavaElement javaElement = findJavaElement(symbol.substring(1, symbol.length() - 1));
+				if (javaElement != null) {
+					return new IHyperlink[] { new JavaElementHyperlink(symbolRegion, symbol, javaElement) };
+				}
+			}
 		}
 		catch (BadLocationException e) {
 		}
 		return null;
+	}
+
+	private IJavaElement findJavaElement(String fullyQualifiedPath) {
+		Path javaPath = new Path(fullyQualifiedPath + ".java");
+		Path classPath = new Path(fullyQualifiedPath + ".class");
+
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceRoot root = workspace.getRoot();
+		IJavaModel javaModel = JavaCore.create(root);
+
+		IJavaProject[] javaProjects;
+		try {
+			javaProjects = javaModel.getJavaProjects();
+			for (int javaProjectIndex = 0; javaProjectIndex < javaProjects.length; javaProjectIndex++) {
+				IJavaProject javaProject = javaProjects[javaProjectIndex];
+				
+				IJavaElement javaElement = javaProject.findElement(classPath);
+				if (javaElement != null)
+					return javaElement;
+				javaElement = javaProject.findElement(javaPath);
+				if (javaElement != null)
+					return javaElement;
+			}
+		}
+		catch (JavaModelException e) {
+		}
+
+		return null;
+	}
+
+	private boolean isTypeName(String symbol) {
+		return symbol.startsWith("<") && symbol.endsWith(">");
 	}
 
 	private class SymbolHyperlink implements IHyperlink {
@@ -77,6 +120,39 @@ public class SymbolHyperlinkDetector implements IHyperlinkDetector {
 				return ((FileEditorInput) input).getFile();
 			}
 			return null;
+		}
+	}
+
+	private class JavaElementHyperlink implements IHyperlink {
+		private IRegion mRegion;
+		private String mLabel;
+		private IJavaElement mJavaElement;
+
+		public JavaElementHyperlink(IRegion region, String label, IJavaElement javaElement) {
+			super();
+			mRegion = region;
+			mLabel = label;
+			mJavaElement = javaElement;
+		}
+
+		public IRegion getHyperlinkRegion() {
+			return mRegion;
+		}
+
+		public String getHyperlinkText() {
+			return mLabel;
+		}
+
+		public String getTypeLabel() {
+			return null;
+		}
+
+		public void open() {
+			try {
+				JavaUI.openInEditor(mJavaElement);
+			}
+			catch (Exception e) {
+			}
 		}
 
 	}
