b44fb4b53c1bc3362b6964e3b256ca9d5128852f
schemeway <schemeway>
2009-06-26 01:08:28 +0000

    When loading a file in the interpreter, the Console View is automatically opened and shown.

--- src/org/schemeway/plugins/schemescript/action/LoadFileAction.java
+++ src/org/schemeway/plugins/schemescript/action/LoadFileAction.java
@@ -13,23 +13,30 @@ import org.schemeway.plugins.schemescript.interpreter.*;
 
 public class LoadFileAction extends SchemeAction {
 
-    public LoadFileAction(SchemeEditor editor) {
-        super(editor);
+	public LoadFileAction(SchemeEditor editor) {
+		super(editor);
 
-        setText("Load File in Interpreter");
-        setToolTipText("Load file in interpreter");
-    }
+		setText("Load File in Interpreter");
+		setToolTipText("Load file in interpreter");
+	}
 
-    public void run() {
-        SchemeEditor editor = getSchemeEditor();
-        if (editor == null)
-            return;
+	public void run() {
+		SchemeEditor editor = getSchemeEditor();
+		if (editor == null)
+			return;
 
-        IEditorInput input = editor.getEditorInput();
-        if (input instanceof FileEditorInput) {
-            FileEditorInput fileInput = (FileEditorInput) input;
-            Interpreter interp = SchemeScriptPlugin.getDefault().getInterpreter();
-            interp.load(fileInput.getFile());
-        }
-    }
+		IEditorInput input = editor.getEditorInput();
+		if (input instanceof FileEditorInput) {
+			FileEditorInput fileInput = (FileEditorInput) input;
+			Interpreter interp = SchemeScriptPlugin.getDefault().getInterpreter();
+			interp.load(fileInput.getFile());
+
+			try {
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(
+						"org.eclipse.ui.console.ConsoleView");
+			}
+			catch (PartInitException e) {
+			}
+		}
+	}
 }
